6f7602e6fa871f8c563ceeb4b927caa06a9c718d
Gabriel Poma <gpoma@24eme.fr>
2025-03-12 12:02:22 +0100

    chore: suppression warnings shellcheck

--- couchdbdoc2git.sh
+++ couchdbdoc2git.sh
@@ -4,22 +4,23 @@
 
 doc=$1
 url=$doc
-if ! echo $url | grep 'http://' > /dev/null; then
+
+if ! echo "$url" | grep 'http://' > /dev/null; then # si pas d'http dans le doc, on construit à partir de config.inc
     url="http://$COUCHDBDOMAIN:$COUCHDBPORT/$COUCHDBBASE/$doc"
-else
-    doc=$(echo $doc | sed 's|http://[^/]*/[^/]*/||')
+else # on récupère la dernière partie de l'url
+    doc=${doc##*/}
 fi
 
 i=0
-mkdir  -p couchdbdoc2git && cd couchdbdoc2git && rm -rf .git/* && rm -f * && git init .
+mkdir  -p couchdbdoc2git && cd couchdbdoc2git && rm -rf .git/* && rm -f -- * && git init .
 curl -s --header 'Accept: application/json' "$url?open_revs=all&revs=true" | jq .[0].ok._revisions.ids.[] > .revisions.json
 
-tac .revisions.json | awk -F '"' '{print $2}'  | while read rev ; do
-	i=$(( $i + 1 )) ;
-	echo $i"-"$rev ;
-	curl -s $url"?rev="$i"-"$rev | jq . > $doc".json" ;
-	git add $doc".json" ;
-	git commit -m "version "$i"-"$rev ;
+tac .revisions.json | awk -F '"' '{print $2}' | while read -r rev ; do
+	i=$(( i + 1 ))
+	echo "$i-$rev"
+	curl -s "$url?rev=$i-$rev" | jq . > "$doc.json"
+	git add "$doc.json"
+	git commit -m "version $i-$rev"
 done
 
-git log -p $doc".json"
+git log -p "$doc.json"
